df4023126a368140cd67a5f7c32b12a1b9ecf2c8
nanduapna <suryavanshiprasad55@gmail.com>
2021-11-08 22:44:15 +0530

    intial commit

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,76 @@
+
+
+for(var i=0;i<document.querySelectorAll(".drum").length;i++){
+document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+
+
+var toPlay=this.innerHTML;
+makeSound(toPlay);
+buttonAnimation(toPlay);
+
+});
+
+
+
+}
+
+
+document.addEventListener("keypress",function(event){
+makeSound(event.key);
+buttonAnimation(event.key);
+})
+
+
+
+function makeSound(key){
+
+switch(key){
+
+case "w":
+    var crash=new Audio("sounds/crash.mp3");
+    crash.play();
+  break;
+ case "a":
+  var crash=new Audio("sounds/kick-bass.mp3");
+    crash.play();
+    break;
+     
+ case "s":
+  var crash=new Audio("sounds/snare.mp3");
+    crash.play();
+    break;
+ case "d":
+       var crash=new Audio("sounds/tom-1.mp3");
+    crash.play();
+    break;
+
+ case "j":
+       var crash=new Audio("sounds/tom-2.mp3");
+    crash.play();
+    break;
+
+ case "k":
+     var crash=new Audio("sounds/tom-3.mp3");
+    crash.play();
+    break;
+ case "l":
+  var crash=new Audio("sounds/tom-4.mp3");
+    crash.play();
+    break;
+default:
+    console.log(toPlay);
+}
+
+}
+
+
+function buttonAnimation(currentKey){
+
+var activeButton=document.querySelector("." + currentKey);
+activeButton.classList.add("pressed");
+
+setTimeout(function(){
+   activeButton.classList.remove("pressed");
+},100);
+
+}
